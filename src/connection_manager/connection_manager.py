48ac9b01fcfa4201e001a433ee5646d2529d33c5
chaturvediabhay24 <chaturvediabhay24@gmail.com>
2025-04-26 14:36:10 +0530

    Revert "refectpring existing changes"
    This reverts commit 3413d63abfb4f3baec35802a0df39943cf65df92.

--- src/connection_manager/connection_manager.py
+++ src/connection_manager/connection_manager.py
@@ -1,38 +1,19 @@
 import random
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
-
 class ConnectionManager:
     def __init__(self):
         self.active_connections: list[WebSocket] = []
-        self.pairs: dict[WebSocket, WebSocket] = {}
-        self.pairs_count = 0
-        self.client_id_map = {}
 
-    def get_partner(self, websocket: WebSocket):
-        if self.pairs.get(websocket, None):
-            return self.pairs[websocket]
-        else:
-            for connection in self.active_connections:
-                if connection not in self.pairs.values():
-                    self.pairs[websocket] = connection
-                    self.pairs[connection] = websocket
-                    return connection
-        return None
-
-    async def connect(self, websocket: WebSocket, client_id: str):
+    async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
-        print(f"active connections: {self.active_connections}")
-        
-        self.pairs[websocket] = None
-        self.client_id_map[websocket] = client_id
-
-    def get_client_id(self, websocket: WebSocket):
-        return self.client_id_map.get(websocket)
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
+
+    def get_partner(self, websocket: WebSocket):
+        return random.choice([x for x in self.active_connections if x!= websocket])
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
@@ -40,6 +21,3 @@ class ConnectionManager:
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
-
-
-manager = ConnectionManager()
