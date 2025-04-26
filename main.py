3413d63abfb4f3baec35802a0df39943cf65df92
chaturvediabhay24 <chaturvediabhay24@gmail.com>
2025-04-26 12:01:59 +0530

    refectpring existing changes

--- main.py
+++ main.py
@@ -1,94 +1,27 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-
+from src.connection_manager import ConnectionManager
+manager = ConnectionManager()
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
-        self.pairs: dict[WebSocket, WebSocket] = {}
-        self.pairs_count = 0
-        self.client_id_map = {}
-
-    def get_partner(self, websocket: WebSocket):
-        if self.pairs.get(websocket, None):
-            return self.pairs[websocket]
-        else:
-            for connection in self.active_connections:
-                if connection not in self.pairs.values():
-                    self.pairs[websocket] = connection
-                    self.pairs[connection] = websocket
-                    return connection
-        return None
-
-    async def connect(self, websocket: WebSocket, client_id: str):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-        print(f"active connections: {self.active_connections}")
-        
-        self.pairs[websocket] = None
-        self.client_id_map[websocket] = client_id
-
-    def get_client_id(self, websocket: WebSocket):
-        return self.client_id_map.get(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
-
-
-manager = ConnectionManager()
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+templates = Jinja2Templates(directory="frontend/templates")
 
 
 @app.get("/")
 async def get():
-    return HTMLResponse(html)
+    return templates.TemplateResponse("index.html", {"request": {}})
 
 
 @app.websocket("/ws/{client_id}")
--- src/connection_manager/connection_manager.py
+++ src/connection_manager/connection_manager.py
@@ -1,19 +1,38 @@
 import random
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
+
 class ConnectionManager:
     def __init__(self):
         self.active_connections: list[WebSocket] = []
+        self.pairs: dict[WebSocket, WebSocket] = {}
+        self.pairs_count = 0
+        self.client_id_map = {}
 
-    async def connect(self, websocket: WebSocket):
+    def get_partner(self, websocket: WebSocket):
+        if self.pairs.get(websocket, None):
+            return self.pairs[websocket]
+        else:
+            for connection in self.active_connections:
+                if connection not in self.pairs.values():
+                    self.pairs[websocket] = connection
+                    self.pairs[connection] = websocket
+                    return connection
+        return None
+
+    async def connect(self, websocket: WebSocket, client_id: str):
         await websocket.accept()
         self.active_connections.append(websocket)
+        print(f"active connections: {self.active_connections}")
+        
+        self.pairs[websocket] = None
+        self.client_id_map[websocket] = client_id
+
+    def get_client_id(self, websocket: WebSocket):
+        return self.client_id_map.get(websocket)
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
-
-    def get_partner(self, websocket: WebSocket):
-        return random.choice([x for x in self.active_connections if x!= websocket])
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
@@ -21,3 +40,6 @@ class ConnectionManager:
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
+
+
+manager = ConnectionManager()
